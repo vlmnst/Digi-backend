3b4a27824e9526a6ee20276d1880a053aefaac23
vlmnst <vale-mansueto@hotmail.com>
2023-11-04 11:41:58 -0300

    feat(backend): ruta pasa solicitar people con y sin filtro

---
+++ src/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express');
+
+const peopleRoutes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/people', peopleRoutes)
+
+
+module.exports = app;
