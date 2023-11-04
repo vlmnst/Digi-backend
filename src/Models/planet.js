f909bcb9bacf44dbd8f3a2c0cdc52052c794b4f5
vlmnst <vale-mansueto@hotmail.com>
2023-11-04 17:00:02 -0300

    feat(backend): model to planets

---
+++ src/Models/planet.js
@@ -0,0 +1,23 @@
+const { UUID } = require('mongodb');
+const { Schema, model } = require('mongoose');
+
+const planetSchema = new Schema({
+    id: UUID,
+    rotation_period: String,
+    orbital_period: String,
+    diameter: String,
+    climate: String,
+    gravity: String,
+    terrain: String,
+    surface_water: String,
+    population: String,
+    residents: String,
+    films: Array,
+    created: String,
+    edited: String,
+    url: String    
+})
+
+const Planet = model('Planet', planetSchema)
+
+module.exports = Planet;
